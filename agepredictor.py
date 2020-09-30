7b81ce48cb6fe74292b1ad4d6ed10dcb96a15fce
Rayan Serrao <53211714+rayanserrao@users.noreply.github.com>
2020-09-30 22:34:27 +0530

    corrected minor errors.

--- agepredictor.py
+++ agepredictor.py
@@ -1,4 +1,4 @@
-age=input('enter your age or year of birth')
+age=input('Enter your age or year of birth')
 age1=int(age)
 present_age=age1
 if age1>2020:
@@ -10,7 +10,7 @@ elif len(age)<=3:
     ageyear=100-age1
     year=2020+ageyear
     if age1>100:
-        print("you are the oldest person alive")
+        print("You are the oldest person alive")
     else:
         ageyear=100-age1
         year=2020+ageyear
@@ -23,7 +23,7 @@ elif len(age)>3:
     ageyear=100-ageyear1
     year=2020+ageyear
     if ageyear1>100:
-        print("you are the oldest person alive")
+        print("You are the oldest person alive")
     else:
 
 
@@ -32,17 +32,17 @@ elif len(age)>3:
         
         print(f"You will become 100 years old after {ageyear} years in {year}")
 
-print("you wish to continue or quit, press c for continue adn q for quit")
+print("you wish to continue or quit, press c for continue and q for quit")
 user_choice=input()
 while(user_choice=='c' or user_choice=='q'):
     if user_choice=='q':
         exit()
     else:
-        print('enter a future year in which you want to see your age')
+        print('Enter a future year in which you want to see your age')
         user_input_year=int(input())
         ageyear1=user_input_year-2020
         ageyear2=present_age+ageyear1
-        print(f"you will become {ageyear2} years old in {user_input_year}")
+        print(f"You will become {ageyear2} years old in {user_input_year}")
         break
 
 
